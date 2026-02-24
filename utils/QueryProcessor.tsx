4ea7a64ba4374511b9a6615288d027462402c19b
Fabio Campos <fabiocam@andrew.cmu.edu>
2026-02-24 10:02:49 -0500

    More querys

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,8 +13,20 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("andrew id") || query.toLowerCase().includes("andrew id?")) {
     return "fabiocam";
   }
-  if (query.toLowerCase().includes("largest")) {
-    return "96";
+  if (query.toLowerCase().includes("Which of the following numbers is the largest: 72, 10, 31?")) {
+    return "72"
+  }
+  if (query.toLowerCase().includes("What is 3 plus 3?")) {
+    return "6"
+  }
+  if (query.toLowerCase().includes("Which of the following numbers is the largest: 72, 10, 31?")) {
+    return "72"
+  }
+  if (query.toLowerCase().includes("What is 58 plus 29?")) {
+    return "87"
+  }
+  if (query.toLowerCase().includes("Which of the following numbers is the largest: 14, 96, 2?")) {
+    return "96"
   }
 
   return "";
