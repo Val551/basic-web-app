cdd808d6564c3a984d270e77bb8d095e57faa49e
Fabio Campos <fabiocam@andrew.cmu.edu>
2026-02-24 10:39:22 -0500

    Scrabble check

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -81,6 +81,21 @@ export default function QueryProcessor(query: string): string {
     return String(result);
   }
 
+  const scrabbleMatch = query.match(/What is the scrabble score of (\w+)\?/i);
+  if (scrabbleMatch) {
+    const scores: Record<string, number> = {
+      a: 1, e: 1, i: 1, o: 1, u: 1, l: 1, n: 1, s: 1, t: 1, r: 1,
+      d: 2, g: 2,
+      b: 3, c: 3, m: 3, p: 3,
+      f: 4, h: 4, v: 4, w: 4, y: 4,
+      k: 5,
+      j: 8, x: 8,
+      q: 10, z: 10,
+    };
+    const score = scrabbleMatch[1].toLowerCase().split("").reduce((sum, c) => sum + (scores[c] || 0), 0);
+    return String(score);
+  }
+
   const anagramMatch = query.match(/Which of the following is an anagram of (\w+): (.+)\?/);
   if (anagramMatch) {
     const sorted = (s: string) => s.toLowerCase().split("").sort().join("");
